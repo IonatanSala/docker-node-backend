a5a03c751510da496797adb3e0ea773b0b238fd1
Ionatan Sala <sala.jonatan1995@gmail.com>
2017-03-31 14:54:24 +0100

    add db

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
 const app = require('express')();
 const mongoose  = require('mongoose');
 const Schema = mongoose.Schema;
-// mongoose.connect('db');
-//
-// const userSchema = new Schema({
-//   name: {
-//     type: String
-//   }
-// })
-//
-// const User = mongoose.model('user', userSchema);
+mongoose.connect('db');
+
+const userSchema = new Schema({
+  name: {
+    type: String
+  }
+})
+
+const User = mongoose.model('user', userSchema);
 
 app.get('/', (req, res, next) => {
-  // const newUser = new User({name: 'Jonatan' + new Date()});
-  // newUser.save()
-  //   .then(u => User.find({}))
-  //   .then(users => res.json(users));
-  res.json({
-    s: 'e'
-  });
+  const newUser = new User({name: 'Jonatan' + new Date()});
+  newUser.save()
+    .then(u => User.find({}))
+    .then(users => res.json(users));
 });
 
 app.listen(process.env.PORT, () => console.log('Listening on port' + process.env.PORT));
